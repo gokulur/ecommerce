781880da00efeba493d5e3dec8a49c30ab8a5ab2
gokulur <gokulkrishnagokul6@gmail.com>
2025-10-25 14:19:20 +0530

    home section completed

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.dashboard, name='dashboard'),  
+    path('register/', views.register_view, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('password_change/', views.password_change_view, name='password_change'),
+    path('password_reset/', views.password_reset_request_view, name='password_reset'),
+    path('reset/<str:token>/', views.password_reset_confirm_view, name='password_reset_confirm'),
 ]
