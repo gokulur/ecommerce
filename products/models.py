20568e636cb211fb7e6a3425cbe895ad3e986a24
gokulur <gokulkrishnagokul6@gmail.com>
2025-10-29 19:00:50 +0530

    product models changed

--- products/models.py
+++ products/models.py
@@ -1,7 +1,30 @@
 from django.db import models
 from django.urls import reverse
+from django.utils.text import slugify
+
+
+class Collection(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(unique=True, blank=True)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to='collections/', blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('collection_detail', args=[self.slug])
+
 
 class Product(models.Model):
+    collection = models.ForeignKey(
+        Collection, on_delete=models.CASCADE, related_name='products', null=True, blank=True
+    )
     name = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
     description = models.TextField(blank=True)
@@ -17,3 +40,4 @@ class Product(models.Model):
 
     def get_absolute_url(self):
         return reverse('product_detail', args=[self.slug])
+
