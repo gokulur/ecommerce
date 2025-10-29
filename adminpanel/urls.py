810b420fc251d9e830349d471cf5d23bff7228df
gokulur <gokulkrishnagokul6@gmail.com>
2025-10-30 01:34:20 +0530

    order section completed

--- adminpanel/urls.py
+++ adminpanel/urls.py
@@ -9,5 +9,11 @@ urlpatterns = [
     path('products/delete/<int:pk>/', views.product_delete, name='admin_product_delete'),
     path('orders/', views.order_list, name='admin_order_list'),
     path('orders/update/<int:pk>/', views.order_update_status, name='admin_order_update'),
+    path('collections/', views.collection_list, name='collection_list'),
+    path('collections/add/', views.collection_add, name='collection_add'),
+    path('collections/edit/<int:pk>/', views.collection_edit, name='collection_edit'),
+    path('collections/delete/<int:pk>/', views.collection_delete, name='collection_delete'),
+    path('admin/customers/', views.admin_customers, name='admin_customers'),
+    
     ]
 
--- adminpanel/views.py
+++ adminpanel/views.py
@@ -6,7 +6,8 @@ from django.contrib import messages
 from products.models import Product
 from orders.models import Order
 from django.contrib.auth.models import User
-
+from products.models import Collection
+from django.utils.text import slugify
 
 # Create your views here.
 @login_required
@@ -86,7 +87,7 @@ def product_delete(request, pk):
 @user_passes_test(admin_only)
 def order_list(request):
     orders = Order.objects.all().select_related('user')
-    return render(request, 'order_list.html', {'orders': orders})
+    return render(request, 'admin_order_list.html', {'orders': orders})
 
 
 @login_required
@@ -101,3 +102,57 @@ def order_update_status(request, pk):
         return redirect('admin_order_list')
     return render(request, 'adminpanel/order_update.html', {'order': order})
 
+
+
+def collection_list(request):
+    collections = Collection.objects.all().order_by('-created_at')
+    return render(request, 'collection_lists.html', {'collections': collections})
+
+
+def collection_add(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        description = request.POST.get('description')
+        image = request.FILES.get('image')
+
+        if not name:
+            messages.error(request, "Name is required.")
+            return redirect('collection_add')
+
+        collection = Collection.objects.create(
+            name=name,
+            slug=slugify(name),
+            description=description,
+            image=image
+        )
+        messages.success(request, f"Collection '{name}' added successfully!")
+        return redirect('collection_list')
+
+    return render(request, 'collection_add.html')
+
+
+def collection_edit(request, pk):
+    collection = get_object_or_404(Collection, pk=pk)
+
+    if request.method == 'POST':
+        collection.name = request.POST.get('name')
+        collection.description = request.POST.get('description')
+        if request.FILES.get('image'):
+            collection.image = request.FILES.get('image')
+        collection.slug = slugify(collection.name)
+        collection.save()
+        messages.success(request, f"Collection '{collection.name}' updated successfully!")
+        return redirect('collection_list')
+
+    return render(request, 'admin/collection_add.html', {'collection': collection})
+
+
+def collection_delete(request, pk):
+    collection = get_object_or_404(Collection, pk=pk)
+    collection.delete()
+    messages.success(request, "Collection deleted successfully.")
+    return redirect('collection_list')
+
+def admin_customers(request):
+    customers = User.objects.all().order_by('-date_joined')
+    return render(request, 'admin_customers.html', {'customers': customers})
