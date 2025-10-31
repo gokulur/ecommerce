ae6e803503badc3e9d2dc5fd07f01a25da7a80cc
gokulur <gokulkrishnagokul6@gmail.com>
2025-11-01 01:03:18 +0530

    product image issue solved

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product, Category 
+from django.core.paginator import Paginator
 def all_collections(request):
     categories = Category.objects.all()
     return render(request, 'all_collections.html', {'categories': categories})
@@ -7,8 +8,11 @@ def all_collections(request):
 
 
 def product_list(request):
-    products = Product.objects.filter(available=True).order_by('-created_at')
-    return render(request, 'product_list.html', {'products': products})
+    products = Product.objects.all()
+    paginator = Paginator(products, 12)   
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'shop/product_list.html', {'page_obj': page_obj})
 
 def product_detail(request, slug):
     product = get_object_or_404(Product, slug=slug, available=True)
