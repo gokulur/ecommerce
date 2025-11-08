043513f3542ffc64a6b05665720a8c39852b1154
gokulur <gokulkrishnagokul6@gmail.com>
2025-11-08 15:19:26 +0530

    conflict solved

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404, redirect
+from django.shortcuts import get_object_or_404, redirect,render
 from .models import Cart, CartItem
 from products.models import Product
 
