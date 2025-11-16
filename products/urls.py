522b2ef64081e0f17941df9a7b66f21340dc84d2
gokulur <gokulkrishnagokul6@gmail.com>
2025-11-17 03:12:49 +0530

    product url updated

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 from . import views
 
+from django.urls import path
+from . import views
+
 urlpatterns = [
-    path('all_collections', views.all_collections, name='all_collections'),
-    path('<slug:slug>/', views.product_detail, name='product_detail'),
+    path('all_collections/', views.all_collections, name='all_collections'),
+
     path('category/<slug:slug>/', views.products_by_category, name='product_by_category'),
+
+ 
+    path('<slug:slug>/', views.product_detail, name='product_detail'),
 ]
+ 
