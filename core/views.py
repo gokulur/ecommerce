9c3a617255321684b2636a2351a2e57feda60309
gokulur <gokulkrishnagokul6@gmail.com>
2025-11-17 15:59:07 +0530

    base.html updated

--- core/views.py
+++ core/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
-from products.models import Product
+from products.models import Product, Collection
 import random
-# Create your views here.
+
 def home(request):
     product_ids = list(Product.objects.values_list('id', flat=True))
     random.shuffle(product_ids)
     selected_ids = product_ids[:8]
-    products = Product.objects.filter(id__in=selected_ids)
-    return render(request, 'base.html', {'products': products})
 
+    # preserve order
+    products = list(Product.objects.filter(id__in=selected_ids))
+    products.sort(key=lambda p: selected_ids.index(p.id))
 
+    collections = Collection.objects.all()[:6]
+
+    return render(request, 'base.html', {
+        'products': products,
+        'collections': collections
+    })
