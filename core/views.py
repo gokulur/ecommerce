08f590f1bd4b7b13cce4549d65b2aa15aea5391d
gokulur <gokulkrishnagokul6@gmail.com>
2025-11-19 18:17:26 +0530

    mini cart added

--- core/views.py
+++ core/views.py
@@ -11,11 +11,24 @@ def home(request):
     # preserve order
     products = list(Product.objects.filter(id__in=selected_ids))
     products.sort(key=lambda p: selected_ids.index(p.id))
-    cart = Cart.objects.get(user=request.user) 
+    
+    # Get cart for logged in users
+    cart = None
+    recommended_products = []
+    if request.user.is_authenticated:
+        cart, created = Cart.objects.get_or_create(user=request.user)
+        
+        # Get recommended products (excluding cart items)
+        cart_product_ids = cart.items.values_list('product_id', flat=True)
+        recommended_products = list(Product.objects.exclude(id__in=cart_product_ids).filter(available=True)[:8])
+        random.shuffle(recommended_products)
+        recommended_products = recommended_products[:4]
+    
     collections = Collection.objects.all()[:6]
 
     return render(request, 'base.html', {
         'products': products,
         'collections': collections,
-        'cart': cart
+        'cart': cart,
+        'recommended_products': recommended_products
     })
