a0ccd47014307000573e549600017c5354935d68
gokulur <gokulkrishnagokul6@gmail.com>
2025-11-22 02:01:50 +0530

    refresh issue solved

--- wishlist/views.py
+++ wishlist/views.py
@@ -12,16 +12,37 @@ def wishlist_page(request):
         'products': products
     })
 
+# @login_required
+# def toggle_wishlist(request, product_id):
+#     product = get_object_or_404(Product, id=product_id)
+#     wishlist, created = Wishlist.objects.get_or_create(user=request.user)
+
+#     if product in wishlist.products.all():
+#         wishlist.products.remove(product)
+#     else:
+#         wishlist.products.add(product)
+#     return redirect(request.META.get('HTTP_REFERER', '/'))
+
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
+
 @login_required
+@require_POST
 def toggle_wishlist(request, product_id):
     product = get_object_or_404(Product, id=product_id)
     wishlist, created = Wishlist.objects.get_or_create(user=request.user)
 
     if product in wishlist.products.all():
         wishlist.products.remove(product)
+        added = False
     else:
         wishlist.products.add(product)
-    return redirect(request.META.get('HTTP_REFERER', '/'))
+        added = True
 
+    return JsonResponse({
+        "success": True,
+        "added": added,
+        "product_id": product_id
+    })
 
 
